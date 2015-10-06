b1ed710e92bd56ae3a25368f15b23edec0220055
himani <himani.sharma@weboniselab.com>
2015-10-06 15:13:51 +0530

    swagger spring mvc demo

---
+++ demo/src/main/java/com/webonise/demo/HomeController.java
@@ -0,0 +1,53 @@
+package com.webonise.demo;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@RequestMapping(value = "/")
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping( method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "welcome", method = RequestMethod.GET)
+	public String displayWelcome( Model model) {
+		
+		String message="Welcome Controller";
+		logger.info(message);
+				
+		model.addAttribute("message", message );
+		
+		return "welcome";
+	}
+	
+	@RequestMapping(value="json")
+	public String jsonApiMethod( Model model){
+	
+		return null;
+	}
+	
+}
